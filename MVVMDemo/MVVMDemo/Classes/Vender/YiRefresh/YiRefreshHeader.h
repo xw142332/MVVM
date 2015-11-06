809ff89d1c72d46f815df319a64b2c3f8a1cf070
xw142332 <xw142332@sina.com>
2015-11-06 13:37:30 +0800

    Update YiRefreshHeader.h

--- MVVMDemo/MVVMDemo/Classes/Vender/YiRefresh/YiRefreshHeader.h
+++ MVVMDemo/MVVMDemo/Classes/Vender/YiRefresh/YiRefreshHeader.h
@@ -1,11 +1,3 @@
-//
-//  YiRefreshHeader.h
-//  YiRefresh
-//
-//  Created by apple on 15/3/6.
-//  Copyright (c) 2015年 coderyi. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
